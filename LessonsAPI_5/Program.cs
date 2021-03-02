ab62be3ced2828927de537df53cf02779b29cd22
Микола Матіос <mikolka09@gmail.com>
2021-03-02 22:43:25 +0200

    02.03.2021

---
+++ LessonsAPI_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LessonsAPI_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = new Task(() => Console.WriteLine("Hello"));
+            task.Start();
+            //task.Wait(); //останавливает основной поток пока не выполнится фоновый
+            Task<int> task2 = new Task<int>(() => 2 * 8);
+            task2.Start();
+            Console.WriteLine(task2.Result);
+            Task task3 = Task.Run(() => 2 + 8).
+                ContinueWith((Task<int> t) => (double)t.Result * t.Result).
+                ContinueWith((Task<double> t) => t.Result / 3).
+                ContinueWith((Task<double> t) => Console.WriteLine(t.Result));
+
+            Parallel.Invoke(() => Console.WriteLine("H"), 
+                            () => Console.WriteLine("E"),
+                            () => Console.WriteLine("L"),
+                            () => Console.WriteLine("L"),
+                            () => Console.WriteLine("O"));
+
+            Console.ReadKey();
+        }
+    }
+}
